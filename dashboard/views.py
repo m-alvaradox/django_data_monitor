04d450474f05cc6ad1f0d2280d9342b18cd27ff9
Austin Estrella <estaus09@gmail.com>
2025-08-13 03:52:35 +0000

    SSR_2

--- dashboard/views.py
+++ dashboard/views.py
@@ -6,9 +6,9 @@ from datetime import datetime
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
-@login_required
+#@login_required
 def index(request):
-
+     
     response = requests.get(settings.API_URL)  # URL de la API
     posts = response.json()  # Convertir la respuesta a JSON
     conteos = posts
@@ -22,20 +22,13 @@ def index(request):
 
     plato_menos_popular = ordenado[-1][0]
 
-    indicadores = [
-        {'nombre': 'Ventas', 'valor': 1200},
-        {'nombre': 'Usuarios', 'valor': 350},
-        {'nombre': 'Visitas', 'valor': 980},
-    ]
-
     data = {
         'title': "La Tonga Manaba Dashboard",
         'total_responses': total_responses,
         'plato_mas_popular': plato_mas_popular,
         'votos_maximos': votos_maximos,
         'plato_menos_popular': plato_menos_popular,
-        'indicadores': indicadores,
+        'conteos': conteos,
     }
         
     return render(request, "dashboard/index.html", data)
-
