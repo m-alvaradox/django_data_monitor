2dbe37daa3748bf2f5677c56737be78631e61139
Mario Alvarado <m_alvaradox@hotmail.com>
2025-08-20 14:23:20 -0500

    Update settings.py

--- backend_analytics_server/settings.py
+++ backend_analytics_server/settings.py
@@ -97,11 +97,11 @@ WSGI_APPLICATION = 'backend_analytics_server.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST', default='localhost'),
-        'PORT': config('DB_PORT', default='3306'),
+        'NAME': os.environ.get('MYSQLDATABASE'),
+        'USER': os.environ.get('MYSQLUSER'),
+        'PASSWORD': os.environ.get('MYSQLPASSWORD'),
+        'HOST': os.environ.get('MYSQLHOST'),
+        'PORT': os.environ.get('MYSQLPORT'),
      }
 }
 
